b5e049e6c7cfa7295efb074efa6c1de080967731
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-13 17:00:36 +0900

    20220613 end

--- java/HelloWorld/src/com/scci/variable/Main.java
+++ java/HelloWorld/src/com/scci/variable/Main.java
@@ -2,6 +2,7 @@ package com.scci.variable;
 
 public class Main {
 	// entry point, 실행 시작점
+	// 클래스 함수
 	public static void main(String[] args) {
 		// 객체를 생성 할 때는 new 생성자();
 		Variable v1 = new Variable("v1");
@@ -11,9 +12,17 @@ public class Main {
 		
 		Vehicle vc1 = new Vehicle(0, "hong", true);
 		Vehicle vc2 = new Vehicle(0, "lim", false);
+		int speed1 = 50;
+		int speed2 = 25;
+		Main.reference(vc1, speed1);
+		Main.reference(vc2, speed2);
+		
+		vc1.showInfo();
+		vc2.showInfo();
 	}
-	
+	// 클래스 함수
 	public static void reference(Vehicle refType, int speed) {
 		refType.speedUp(speed);
+		speed = speed + 1;
 	}
 }
