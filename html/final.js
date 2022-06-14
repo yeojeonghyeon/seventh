41998e48084f0b4d847295375cd3a289ebef7f79
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-14 16:37:42 +0900

    202206141637

--- html/final.js
+++ html/final.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function(){
     var secondObj = document.querySelector("#secondOperand");
     var resultObj = document.querySelector("#resultIpt");
     document.querySelector("#addBtn").addEventListener("click", function(){
-        var result = firstObj.value + secondObj.value;
+        var result = Number(firstObj.value) + Number(secondObj.value);
         resultObj.value = result;
     });
     document.querySelector("#subtractBtn").addEventListener("click", function(){
+        var result = parseInt(firstObj.value) - parseInt(secondObj.value);
+        resultObj.value = result;
     });
     document.querySelector("#multiplyBtn").addEventListener("click", function(){
+        var result = parseFloat(firstObj.value) * parseFloat(secondObj.value);
+        resultObj.value = result;
     });
     document.querySelector("#divideBtn").addEventListener("click", function(){
+        var result = parseFloat(firstObj.value) / parseFloat(secondObj.value);
+        resultObj.value = result;
     });
 });
