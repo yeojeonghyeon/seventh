3749208ff35628f1657354dd4113b0f126098184
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-14 14:56:52 +0900

    202206141456

--- html/thirdScript.js
+++ html/thirdScript.js
@@ -1,10 +1,17 @@
+"use strict"
 var gName = 0;
 
 var x = 1, y = 5;
 
-result = add(x, y);
+var result = add(x, y);
 
+// 콘솔에 파라미터의 값을 출력 해 주는 함수.
 console.log(result);
+
+console.log(x+"+"+y+"="+result);
+console.log(x+'+'+y+'='+result);
+// best
+console.log(`${x}+${y}=${result}`);
 
 function add(x, y){
     return x+y;
