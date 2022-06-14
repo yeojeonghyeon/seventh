4947333a74ea5869cee5eb9e906a75bed27face3
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-14 11:34:25 +0900

    202206141134

--- html/script.js
+++ html/script.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
+    var iptObj = document.querySelector("#dpIpt");
     document.querySelector("#firstBtn").addEventListener("click", function(){
-        alert(this.innerText);
+        iptObj.value = this.innerText;
     });
     document.querySelector("#secondBtn").addEventListener("click", function(){
-        alert(this.innerText);
+        iptObj.value = this.innerText;
     });
 });
+
+(function(){
+    function outer(){ // 외부 함수
+        var i = 0 ;
+        inner();
+        alert(i);
+        function inner(){ // 내부 함수
+            i = i + 1;
+        }
+    }
+    outer();
+})();
