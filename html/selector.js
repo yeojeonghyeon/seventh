461886a9f039a807955e68174cc25b13f78a28e3
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-15 16:16:01 +0900

    20220615

--- html/selector.js
+++ html/selector.js
@@ -1,7 +1,9 @@
+var i = 0;
 document.addEventListener("DOMContentLoaded", function(){
     var olObj = document.querySelector("section > article.container > div.content > ol");
     olObj.addEventListener("click", olClickFunc);
     var dpIptObj = document.querySelector("#dpIpt");
+
     function olClickFunc(event){
         // event.target 객체를 이용
         // event.target 프로퍼티는 이벤트가 발생한 tag(요소, 객체)
