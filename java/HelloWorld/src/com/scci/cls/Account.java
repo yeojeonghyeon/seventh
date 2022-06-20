9815723a04400ab572ca0356f675ec3c13a42b11
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-20 13:45:08 +0900

    20220620

--- java/HelloWorld/src/com/scci/cls/Account.java
+++ java/HelloWorld/src/com/scci/cls/Account.java
@@ -8,8 +8,13 @@ public class Account {
 		this.balance = balance;
 	}
 	// 출금
+	// withdraw 함수는 예외를 던지는 함수.
+	// 함수 파라미터 자리 뒤에 throws Exception 구문이 들어간 의미는
+	// 이 함수는 해당 예외를 던질 수 있다는 의미
+	// 예외처리 : 예외가 발생 했을 때 실행(흐름)을 제어 하는 것
 	public int withdraw(int money) throws Exception {
 		if ( this.balance - money < 0 ) {
+			// 예외를 던진는 구문
 			throw new Exception("balance not enough" + this.balance);
 		}
 		this.balance = this.balance - money;
@@ -18,5 +23,11 @@ public class Account {
 	// 예금
 	public void deposit(int money) {
 		this.balance = this.balance + money;
+	}
+	
+	public String getInfo() {
+		// accHolder의 name 필드(인스턴스 변수)를 직접 접근하지 못한 이유는
+		// private으로 접근지시자가 지정 되어 있어서, 해당 클래스(AccountHolder) 외부에서는 접근이 불가능
+		return accHolder.getName() + " " + balance; 
 	}
 }
--- java/HelloWorld/src/com/scci/cls/Main.java
+++ java/HelloWorld/src/com/scci/cls/Main.java
@@ -7,16 +7,29 @@ public class Main {
 		Account acc1 = new Account(new AccountHolder("smith", "suncheon", "01012345678"), 10000);
 		Account acc2 = new Account(new AccountHolder("david", "yeosu", "01078901234"), 20000);
 		
+		acc1.deposit(3000);
+		// 예외 처리기 : 예외가 발생 했을 때 어떻게 처리 할 것인가 ( 흐름을 어떻게 제어 할 것인가 )
+		// try 블럭 안에서 예외가 발생하면 
+		// catch 문으로 제어(흐름)가 이동하고, 발생한 예외와 같으면
+		// catch block으로 실행이 이동
+		try {
+			acc2.withdraw(5000);
+			acc1.withdraw(13100);
+			System.out.println(11111111);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 		ArrayList<Account> lst = new ArrayList<Account>();
 		lst.add(acc1);
 		lst.add(acc2);
 		
-		Main.showFiltered(lst, 15000);
+		Main.showFiltered(lst, 10000);
 	}
 	public static void showFiltered(ArrayList<Account> src, int filter) {
 		for(Account acc : src) {
 			if ( acc.balance >= filter ) {
-				acc.showInfo();
+				System.out.println(acc.getInfo());
 			}
 		}
 	}
