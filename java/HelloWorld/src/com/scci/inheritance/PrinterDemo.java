badd25b1cefac2ea3c7316e2c5244205ad28f7dd
mrclumsy <yeojeonghyeon@gmail.com>
2022-06-28 11:07:49 +0900

    20220627

--- java/HelloWorld/src/com/scci/inheritance/PrinterDemo.java
+++ java/HelloWorld/src/com/scci/inheritance/PrinterDemo.java
@@ -1,7 +1,16 @@
 package com.scci.inheritance;
 
+import java.util.ArrayList;
+
 public class PrinterDemo {
 	public static void main(String[] args) {
+		ArrayList<Printer> list = new ArrayList<Printer>();
+		list.add(new InkjetPrinter("ink450j", "samsung", "123456-789", 200, false, true));
+		list.add(new LaserPrinter("laserjet407i", "lg", "7997-123689", 450, false, "BigSize"));
 		
+		for(Printer p : list) {
+			String contents = p.getInfo();
+			p.print(contents);
+		}
 	}
 }
