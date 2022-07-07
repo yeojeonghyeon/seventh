242beb403457ac36fb1f5a65dc4439b79747b0c9
mrclumsy <yeojeonghyeon@gmail.com>
2022-07-07 14:05:38 +0900

    20220707

--- java/HelloWorld/src/com/scci/generic/UnOrderedPair.java
+++ java/HelloWorld/src/com/scci/generic/UnOrderedPair.java
@@ -1,6 +1,30 @@
 package com.scci.generic;
 
-public class UnOrderedPair<K, V extends Number> implements Pair<K, V> {
+
+class UnOrderedDemo {
+	public static void main(String[] args) {
+		Pair<String, Integer> p;
+		UnOrderedPair<String, Parent> up = new UnOrderedPair<>("k178", new Parent("parent"));
+		UnOrderedPair<String, Child> uc = new UnOrderedPair<>("k179", new Child("child", "sc city"));
+		UnOrderedPair<String, Parent> upp = new UnOrderedPair<>("k180", new Child("ccild", "ys city"));
+		
+		UnOrderedPair<?, ?> uAll = up;
+		uAll = uc;
+		uAll = upp;
+		Object key = uAll.getKey();
+		Parent v = uAll.getValue();
+		
+		String s = up.getKey();
+		Parent pp = up.getValue();
+		
+		uc.getKey();
+		uc.getValue();
+	}
+}
+
+// type parameter V는 Parent의 하위 타입만 type argument(concrete type) 올 수 있다
+// V extends Parent : bounded type parameter
+public class UnOrderedPair<K, V extends Parent> implements Pair<K, V> {
 	K key;
 	V value;
 	@Override
@@ -16,5 +40,19 @@ public class UnOrderedPair<K, V extends Number> implements Pair<K, V> {
 	public UnOrderedPair(K key, V value) {
 		this.key = key;
 		this.value = value;
+	}
+}
+
+class Parent {
+	String name;
+	public Parent(String name) {
+		this.name = name;
+	}
+}
+class Child extends Parent {
+	String address;
+	public Child(String name, String address) {
+		super(name);
+		this.address = address;
 	}
 }
