20d282ff2002655ed98c0661c1fcc1872b2e9d68
mrclumsy <yeojeonghyeon@gmail.com>
2022-07-13 10:58:48 +0900

    20220713

--- nodejs/simple-server/index.js
+++ nodejs/simple-server/index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/form.html');
 });
 
+app.get('/ajax', (req, res)=>{
+    res.sendFile(__dirname + '/ajax.html');
+});
+
 app.get('/hello', function(req, res){
     res.send("Hello World!!!!!!!!!");
 });
@@ -23,6 +27,14 @@ app.post('/register', (req, res)=>{
 
 app.get('/list', function(req, res){
     const result = {list:["javascript", "java", "database", "spring", "reactjs", "html5"]};
+    res.json(result);
+});
+
+app.get('/getSingers', function(req, res){
+    const result = {signers:[{no: 1, singerName: "BTS", NumOfAlbum: 3, company: "하이브"},
+                             {no: 2, singerName: "아이유", NumOfAlbum: 5, company: "이담엔터테인먼트"},
+                             {no: 3, singerName: "신승훈", NumOfAlbum: 11, company: "도로시컴퍼니"}
+                            ]};
     res.json(result);
 });
 
