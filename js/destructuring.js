4b3f699352697ef40444e853b059e73134d777b0
mrclumsy <yeojeonghyeon@gmail.com>
2022-07-14 15:20:50 +0900

    20220714

--- js/destructuring.js
+++ js/destructuring.js
@@ -13,3 +13,20 @@ zoo(obj);
 function zoo({name, birth}){
     console.log(name, birth);
 }
+
+// new Array()
+// todos.__proto__ <= Array.prototype
+const todos = [
+    {id: 1, content: 'HTML5', completed: true},
+    {id: 2, content: 'CSS3', completed: true},
+    {id: 3, content: 'es5,6', completed: false},
+    {id: 4, content: 'jsp/servlet', completed: true}
+];
+// completed는 배열 객체가 가지고 있는 요소들의 property, completed를 parameter로 사용
+// 모던 자바스크립트에서 자주 사용 될 수 있는 문법
+const completedSubjects = todos.filter(({completed})=>completed);
+
+const originSubjects = todos.filter((element)=>element.completed);
+
+console.log(completedSubjects, originSubjects);
+
