326eedb5f9471b6de4a1e52205c045d95f0e54f6
mrclumsy <yeojeonghyeon@gmail.com>
2022-07-18 15:27:27 +0900

    20220718

--- java/HelloWorld/src/com/scci/generic4/ComparableDemo.java
+++ java/HelloWorld/src/com/scci/generic4/ComparableDemo.java
@@ -1,5 +1,10 @@
 package com.scci.generic4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class ComparableDemo {
 	public static void main(String[] args) {
 		Integer[] numArr = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -7,6 +12,45 @@ public class ComparableDemo {
 		
 		int result = ComparableDemo.countGreaterThan(numArr, basis);
 		System.out.println(result);
+		
+		Person[] pArr = new Person[] {
+				new Person(1953),
+				new Person(1977),
+				new Person(1533),
+				new Person(1399)
+		};
+		Person pBasis = new Person(1733);
+		
+		result = ComparableDemo.countGreaterThan(pArr, pBasis);
+		System.out.println(result);
+		// 배열의 요소로 Student 객체의 레퍼런스를 담는 배열 선언
+		// 클래스 타입 배열 선언
+		Student[] sArr = new Student[] {
+				new Student('A'),
+				new Student('B'),
+				new Student('C'),
+				new Student('A'),
+				new Student('F')
+		};
+		// 'D'
+		Student sBasis = new Student('D');
+		result = ComparableDemo.countGreaterThan(sArr, sBasis);
+		System.out.println(result);
+		
+		List<Person> personLst = new ArrayList<Person>(Arrays.asList(
+				new Person(1953),
+				new Person(1555),
+				new Person(1933),
+				new Person(1333),
+				new Person(2021),
+				new Person(2017)
+				));
+		Predicate<Person> predicate = (p) -> {
+			return p.birth > 1500 && p.birth < 2022;
+		};
+		
+		List<Person> resultLst = ComparableDemo.test(personLst, predicate);
+		// resultLst, enhanced for를 사용하여 각 요소의 birth를 출력 하라
 	}
 	
 	public static <T extends Comparable<T>> int countGreaterThan(T[] arr, T basis){
@@ -17,6 +61,16 @@ public class ComparableDemo {
 			}
 		}
 		return count;
+	}
+	
+	public static <T> List<T> test(List<T> src, Predicate<T> predicate){
+		List<T> result = new ArrayList<T>();
+		for(T ele : src) {
+			if ( predicate.test(ele) ) {
+				result.add(ele);
+			}
+		}
+		return result;
 	}
 }
 
