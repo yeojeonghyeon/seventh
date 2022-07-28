76536f14c61af4c32bc9d1db1626e09830bc32ee
mrclumsy <yeojeonghyeon@gmail.com>
2022-07-28 17:00:28 +0900

    20220728

---
+++ java/swag/src/main/java/com/scci/swag/App.java
@@ -0,0 +1,28 @@
+package com.scci.swag;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("resource/beans.xml");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+	}
+}
+
+class HelloWorld {
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+}
