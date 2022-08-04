f99c0d36519a4e975793211ee53347c058860a27
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-04 13:43:27 +0900

    20220804

--- servlet/src/main/java/com/scci/servlet/FileUploadServlet.java
+++ servlet/src/main/java/com/scci/servlet/FileUploadServlet.java
@@ -21,29 +21,5 @@ public class FileUploadServlet extends HttpServlet{
 	public static String UPLOAD_DIRECTORY = "upload";
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists())
-            uploadDir.mkdir();
-
-        try {
-            String fileName = "";
-            for (Part part : request.getParts()) {
-                fileName = getFileName(part);
-                part.write(uploadPath + File.separator + fileName);
-            }
-            request.setAttribute("message", "File " + fileName + " has uploaded successfully!");
-        } catch (FileNotFoundException fne) {
-            request.setAttribute("message", "There was an error: " + fne.getMessage());
-        }
-        getServletContext().getRequestDispatcher("/views/result.jsp").forward(request, response);
 	}
-	
-    private String getFileName(Part part) {
-        for (String content : part.getHeader("content-disposition").split(";")) {
-            if (content.trim().startsWith("filename"))
-                return content.substring(content.indexOf("=") + 2, content.length() - 1);
-        }
-        return "default.file";
-    }
 }
