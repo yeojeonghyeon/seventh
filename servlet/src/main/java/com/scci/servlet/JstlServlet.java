649a2b4259aac0d4a371a177322aac419543886e
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-08 15:01:30 +0900

    20220808

--- servlet/src/main/java/com/scci/servlet/JstlServlet.java
+++ servlet/src/main/java/com/scci/servlet/JstlServlet.java
@@ -38,10 +38,20 @@ public class JstlServlet extends HttpServlet {
 	
 	private static List<Employee> getEmployees(){
 		List<Employee> list = new ArrayList<Employee>();
+		list.add(new Employee("가나다","100","101","총무과","2016"));
+		list.add(new Employee("김술집","101","101","총무과","1996"));
+		list.add(new Employee("니나노","102","101","총무과","1987"));
+		list.add(new Employee("분식집","103","101","총무과","2020"));
+		list.add(new Employee("맛있다","104","101","총무과","2022"));
 		return list;
 	}
 	private static List<Department> getDepartments(){
 		List<Department> list = new ArrayList<Department>();
+		list.add(new Department("101","총무과"));
+		list.add(new Department("102","경리과"));
+		list.add(new Department("103","홍보과"));
+		list.add(new Department("104","기획과"));
+		list.add(new Department("105","산림과"));
 		return list;
 	}
 }
