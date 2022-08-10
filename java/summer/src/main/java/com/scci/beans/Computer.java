f8758767b98d48232f81bede5ea063623abddd5d
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-10 16:28:24 +0900

    20220810

---
+++ java/summer/src/main/java/com/scci/beans/Computer.java
@@ -0,0 +1,19 @@
+package com.scci.beans;
+
+public class Computer {
+	private String manufacture;
+	private String serialNo;
+	public Computer() {}
+	public String getManufacture() {
+		return manufacture;
+	}
+	public void setManufacture(String manufacture) {
+		this.manufacture = manufacture;
+	}
+	public String getSerialNo() {
+		return serialNo;
+	}
+	public void setSerialNo(String serialNo) {
+		this.serialNo = serialNo;
+	}
+}
--- java/summer/src/main/java/com/scci/summer/App.java
+++ java/summer/src/main/java/com/scci/summer/App.java
@@ -3,10 +3,15 @@ package com.scci.summer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.scci.beans.Computer;
+
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		HelloWorld helloWorld = (HelloWorld)context.getBean("helloWorld");
 		System.out.println(helloWorld);
+		Computer computer = (Computer)context.getBean("computer");
+		System.out.println(computer.getManufacture());
+		System.out.println(computer.getSerialNo());
 	}
 }
