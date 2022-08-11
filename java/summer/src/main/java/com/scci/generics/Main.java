35a6c9038294fb6f2bbc0740f7bd047a0a01913f
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-11 15:13:17 +0900

    20220810

--- java/summer/src/main/java/com/scci/generics/Main.java
+++ java/summer/src/main/java/com/scci/generics/Main.java
@@ -3,7 +3,22 @@ package com.scci.generics;
 import java.util.function.Consumer;
 
 public class Main {
-	public static void forEach(Consumer<? super Child> op) {
+	public static void main(String[] args) {
+		Main.foo(new Consumer<Child>() {
+			public void accept(Child t) {
+				
+			}
+		}, new Child());
 		
+		Main.foo((t)->{}, new Child());
+	}
+	// void accept(T t)
+	// Consumer<? super Child>'s sub types are
+	// Consumer<Child>, Consumer<Parent>, Consumer<Object>
+	public static void foo(Consumer<? super Child> op, Child child) {
+		op.accept(child);
+	}
+	public static void zoo(Consumer<? extends Parent> op, Parent parent) {
+		//
 	}
 }
