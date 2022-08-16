d70b3ed6149f1f07880b2231687cdc27308abb89
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-16 10:10:43 +0900

    20220816

---
+++ java/summer/src/main/java/com/scci/aop/Main.java
@@ -0,0 +1,15 @@
+package com.scci.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.scci.beans.Computer;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Computer computer = (Computer)context.getBean("computer");
+		computer.setManufacture("hp");
+		computer.getManufacture();
+	}
+}
