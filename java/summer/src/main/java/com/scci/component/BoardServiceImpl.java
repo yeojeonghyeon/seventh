04ef57df53b5fdcbd79b887077bea8c493d74455
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-17 15:14:22 +0900

    20220817

--- java/summer/src/main/java/com/scci/component/BoardServiceImpl.java
+++ java/summer/src/main/java/com/scci/component/BoardServiceImpl.java
@@ -2,13 +2,21 @@ package com.scci.component;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BoardServiceImpl implements BoardService {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	BoardDao boardDao; // boardDao bean이 주입
 	public void setBoardDao(BoardDao boardDao) {
 		this.boardDao = boardDao;
 	}
 	@Override
 	public List<Post> getPosts() {
+		logger.debug("BoardServiceImpl's getPosts called!! level:debug");
+		logger.info("BoardServiceImpl's getPosts called!! level:info");
+		logger.warn("BoardServiceImpl's getPosts called!! level:warn");
+		logger.error("BoardServiceImpl's getPosts called!! level:error");
 		return boardDao.getPosts();
 	}
 }
--- java/summer/src/main/java/com/scci/component/Main.java
+++ java/summer/src/main/java/com/scci/component/Main.java
@@ -2,6 +2,8 @@ package com.scci.component;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
