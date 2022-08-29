e0df1288fd13472e4b858319d6b9511fd86dd7b9
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-29 14:08:15 +0900

    20220829

--- java/UsingDynamic/src/main/java/com/scci/dao/TestDAOImpl.java
+++ java/UsingDynamic/src/main/java/com/scci/dao/TestDAOImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class TestDAOImpl implements TestDAO{
 	@Autowired
-	@Qualifier("sqlSessionOracle")
 	private SqlSession sqlSession;
 	
 
