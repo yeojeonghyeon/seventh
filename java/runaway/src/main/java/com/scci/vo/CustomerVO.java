2fe68561c8cbc5a3ba81abdc70d034ea184a621d
mrclumsy <yeojeonghyeon@gmail.com>
2022-08-30 15:24:41 +0900

    20220830

---
+++ java/runaway/src/main/java/com/scci/vo/CustomerVO.java
@@ -0,0 +1,32 @@
+package com.scci.vo;
+
+public class CustomerVO {
+	private String custId;
+	private String custNm;
+	private String address;
+	private String phone;
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getCustNm() {
+		return custNm;
+	}
+	public void setCustNm(String custNm) {
+		this.custNm = custNm;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
